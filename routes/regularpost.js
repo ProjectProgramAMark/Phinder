a9ca86a9187c48bff33264e4d274a1133fe1bfee
ProjectProgramAMark <projectprogramamark@gmail.com>
2016-03-05 13:32:18 -0500

    swag is a commitment

---
+++ routes/regularpost.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST personality stuff. */
+router.post('/', function(req, res, next) {
+  console.log(req.body.you);
+
+  res.render('index', { title: 'Phinder' });
+});
+
+module.exports = router;
